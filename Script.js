d50c866bee3e102df78a1abab218ee40f13b6cff
Moritz <55063400+Mori-TM@users.noreply.github.com>
2024-09-04 19:38:49 +0200

    Improvements

--- Script.js
+++ Script.js
@@ -1,5 +1,45 @@
 let Fullscreen = true;
 
+fetch('Posts.json')
+  .then(response => response.json())
+  .then(data => {
+    const postContainer = document.getElementById('HomeTab');
+    
+    // Loop through each post in the JSON data
+    data.posts.forEach(post => {
+      // Create the figure element
+      const figure = document.createElement('figure');
+      figure.classList.add('PostBlock');
+
+      // Create and append the figcaption for the title
+      const figcaptionTitle = document.createElement('figcaption');
+      figcaptionTitle.textContent = post.title;
+      figure.appendChild(figcaptionTitle);
+
+      // Create and append the image element
+      const img = document.createElement('img');
+      img.setAttribute('loading', 'lazy');
+      img.src = post.image;
+      img.alt = post.title;
+      figure.appendChild(img);
+
+      // Create and append the figcaption for the description
+      const figcaptionDescription = document.createElement('figcaption');
+      figcaptionDescription.classList.add('PostUnderText');
+      figcaptionDescription.textContent = post.description;
+      figure.appendChild(figcaptionDescription);
+
+      // Append the figure to the post container
+      const PostBackground = document.createElement('div');
+      PostBackground.classList.add('PostBackground');
+
+      PostBackground.appendChild(figure);
+
+      postContainer.appendChild(PostBackground);
+    });
+  })
+  .catch(error => console.error('Error fetching posts:', error));
+
 function OpenPage(PageName) {
     // Hide all elements with class="tabcontent" by default 
     var i, tabcontent, tablinks;
@@ -96,6 +136,11 @@ function ChangeToActive(Href) {
     homeElement.classList.add('active');
 }
 
+function SetPageHeadline(Headline)
+{
+    document.getElementById("Headline").innerHTML = Headline;
+}
+
 function CloseMenuBar() {
     var x = document.getElementById("myTopnav");
     x.className = "topnav";
@@ -109,6 +154,7 @@ function MGButton() {
 function HomeButton() {
     ResestButtonColor();
     ChangeToActive("home");
+    SetPageHeadline("My Projects");
     CloseMenuBar();
     OpenPage("HomeTab");
 }
@@ -116,6 +162,7 @@ function HomeButton() {
 function ContactButton() {
     ResestButtonColor();
     ChangeToActive("contact");
+    SetPageHeadline("Contact Me");
     CloseMenuBar();
     OpenPage("ContactTab");
 }
@@ -123,6 +170,7 @@ function ContactButton() {
 function AboutButton() {
     ResestButtonColor();
     ChangeToActive("about");
+    SetPageHeadline("About Me");
     CloseMenuBar();
     OpenPage("AboutTab");
 }
